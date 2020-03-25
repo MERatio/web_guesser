b06b3a01c8e36aa58eec652d52d445439e217dfe
Mark Edward Ratio <markedwardratio@gmail.com>
2020-03-25 18:34:07 +0800

    Colorize background based on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,9 +6,11 @@ require 'sinatra/reloader'
 set :secret_number, rand(101)
 
 get '/' do
-  guess = params['guess'].to_i
-  message = check_guess(guess, settings.secret_number)
-  erb :index, locals: { number: settings.secret_number, message: message }
+  guess = params[:guess].to_i
+  evaluation = check_guess(guess, settings.secret_number)
+  erb :index, locals: { number: settings.secret_number,
+                        message: evaluation[:message],
+                        bg_color: evaluation[:bg_color] }
 end
 
 def check_guess(guess, secret_number)
@@ -17,22 +19,27 @@ def check_guess(guess, secret_number)
   elsif guess < secret_number
     low_guess(guess, secret_number)
   else
-    "You got it right! The SECRET NUMBER is #{secret_number}"
+    correct_guess(secret_number)
   end
 end
 
 def high_guess(guess, secret_number)
   if guess > secret_number + 5
-    'Way too high!'
+    { message: 'Way too high!', bg_color: '#f33' }
   else
-    'Too high!'
+    { message: 'Too high!', bg_color: '#f77' }
   end
 end
 
 def low_guess(guess, secret_number)
   if guess < secret_number - 5
-    'Way too low!'
+    { message: 'Way too low!', bg_color: '#f33' }
   else
-    'Too low!'
+    { message: 'Too low!', bg_color: '#f33' }
   end
+end
+
+def correct_guess(secret_number)
+  { message: "You got it right! The SECRET NUMBER is #{secret_number}",
+    bg_color: '#4f4' }
 end
